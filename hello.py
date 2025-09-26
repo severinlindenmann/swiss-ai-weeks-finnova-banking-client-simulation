10b2a1f5fd73129118b4cf06f41094a65f35e6bd
severinlindenmann <github@severin.io>
2025-09-26 10:20:43 +0200

    first example swisscom api

---
+++ hello.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+
+client = openai.OpenAI(
+    api_key=os.getenv("SWISS_AI_PLATFORM_API_KEY"),
+    base_url="https://api.swisscom.com/layer/swiss-ai-weeks/apertus-70b/v1"
+)
+
+stream = client.chat.completions.create(
+    model="swiss-ai/Apertus-70B",
+    messages=[
+        {"role": "system", "content": "You are a travel agent. Be descriptive and helpful"},
+        {"role": "user", "content": "What are the best places to visit in Switzerland?"}
+    ],
+    stream=True
+)
+
+for chunk in stream:
+    print(chunk.choices[0].delta.content or "", end="", flush=True)
