c02eedfbc2b423c80c1a8ce7456e023f2c32885f
severinlindenmann <github@severin.io>
2025-09-26 14:11:15 +0200

    update

--- persona-generator/data.py
+++ persona-generator/data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def load_demographie_csv():
   csv_path = Path(__file__).parent / "../data/Demographie/datax.csv"
-  df = pd.read_csv(csv_path)
+  df = pd.read_csv(csv_path, low_memory=False)
   return df
 
 def filter_df_by_params(df, params):
